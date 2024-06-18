633763dcff3681eefbd42147a18039ebea0072cd
Ocsovszki Tamás <ocsovszkit@expressone.hu>
2024-06-18 10:14:17 +0200

    Menu System added and Published

--- Torpedo/obj/Debug/net8.0/Torpedo.AssemblyInfo.cs
+++ Torpedo/obj/Debug/net8.0/Torpedo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Torpedo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93e6e1e1ace49562e6785d6dfea0ad6578c9786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606cbfd05dbb84c9faefc9800f7929c80759fe06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Torpedo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Torpedo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Torpedo/obj/Release/net8.0/win-x64/Torpedo.AssemblyInfo.cs
+++ Torpedo/obj/Release/net8.0/win-x64/Torpedo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Torpedo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af2c72f75bc167c3fcfe424ea33ad327c44add8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606cbfd05dbb84c9faefc9800f7929c80759fe06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Torpedo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Torpedo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
